7738a7476ff6a3b324a07e3d04e6a32adeca60bd
Muriel <muriel.mangubat@gmail.com>
2017-12-04 19:43:29 -0600

    another function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,6 +13,9 @@ var connection = mysql.createConnection({
 
 });
 
+var stock = [];
+
+
 // When connecting to database will throw error if unable, call displayDb()
 connection.connect(function(err){
   if (err) throw err;
@@ -23,7 +26,8 @@ connection.connect(function(err){
 function displayDb() {
     connection.query("SELECT * FROM products", function(err, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].id + " | Product: " + res[i].product_name + " | Department: " + res[i].department_name + " | Price $" + res[i].price + " | Stock Count: " + res[i].stock_quantity);  
+      console.log(res[i].id + " | Product: " + res[i].product_name + " | Department: " + res[i].department_name + " | Price $" + res[i].price + " | Stock Count: " + res[i].stock_quantity); 
+  
     };
     console.log("-----------------------------------");
     whatItem();
@@ -35,14 +39,47 @@ function whatItem(){
     {
         name: 'name',
         message: 'Which item number would you like to purchase?'
-    },
-    {
-        name: 'stockNumber',
-        message: 'How many units would you like to purchase?'
-    },
-    ]).then(function(answer){
+    }]).then(function(answer){
+
+         connection.query('SELECT * FROM products WHERE id =' + answer.stockNumber, function(error, results, fields) {
+                if (error) throw error;
+                //id.push(data.id);
+                stock.push(results[0].stock_quantity);
+                //price.push(results[0].price);
+      })
         insertItem(answer.name, answer.stockNumber);
+
+     
     });
 }
+function checkOut(){
+      inquirer.prompt([
+      {
+        name: 'stockNumber',
+        message: 'How many units would you like to purchase?'
+    }]).then(function(answer){
+           if (answer.stockNumber > stock[0] ){
+           console.log('Insufficient quantity!' + '\n' + 'We only have ' + stock[0] + ' units in stock'), goBack();
+         }else {  }
+        
+      })
+
+}
+
+function goBack() {
+  inquirer.prompt([{
+      type: "confirm",
+      name: "yn",
+      message: 'Would you like to go back?',
+      choices: ["Choice A", new inquirer.Separator(), "choice B"]
+
+  }]).then(function(data) {
+      if (data.yn == true) {
+          displayDb();
+      } else {
+          console.log('Bye Bye, Come back soon!'), connection.end();
+      }
+  });
 
 
+}
